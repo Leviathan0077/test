c67010abdd101ff3f2b48ee7cfbb2a0c488f3eda
Prateek <prateekdubey2663@gmail.com>
2023-11-02 17:03:35 +0700

    abcd

---
+++ feature.js
@@ -0,0 +1,12 @@
+/*function DoSomething(){
+    console.log("feature file");
+
+
+}
+
+module.export = DoSomething; //EXPORTS THE FUNCTION DoSomething*/
+
+var Dosomething = require('./index.js');
+DoSomething();
+
+ 
